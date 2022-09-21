eaea12f8c4fbc3934a5bfd59d8d16441110cc08e
Onion-Man <siddpal04@gmail.com>
2022-09-21 21:12:12 +0530

    inital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-semibold text-cyan-500 underline">
+        Hello there
+      </h1>
+    </div>
+  )
+}
+
+export default App
