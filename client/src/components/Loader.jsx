ce7c8e0a2a0955f055c00297db3af14eb8096dfa
Onion-Man <siddpal04@gmail.com>
2022-09-22 21:41:43 +0530

    loader component build complete

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center py-3 ">
+      <div class="flex items-center gap-2 text-gray-500">
+        <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-red-500"/>
+      </div>
+    </div>
   )
 }
 
 export default Loader
+
+    
